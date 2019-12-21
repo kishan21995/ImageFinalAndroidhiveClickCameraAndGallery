4eae10a33fe5d0feedb74c575d30c268686f51fd
Kishan <kishangupta21995@gmail.com>
2019-12-21 15:31:32 -0500

    Initial commit

---
+++ app/src/main/java/com/app/myapplication/MyGlideModule.java
@@ -0,0 +1,8 @@
+package com.app.myapplication;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule //most importent way add depandency
+public class MyGlideModule extends AppGlideModule {
+}
